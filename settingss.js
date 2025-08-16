bacf2c1f1736d21cf8587511a8b5df1ea7cb097d
banksherman <banksherman8@gmail.com>
2025-08-17 00:10:53 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~xylziIKI#SrBxs1iXOP1QFIVu4YwXQJHqb9lh1Ud4UFahhMvzjKs",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "256726212154", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
